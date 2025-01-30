46b6ab79d17d55e41d899dc88ba0d6c0d10215fc
Jonathan Eberle <jonathan.eberle@gmx.de>
2025-01-30 16:51:02 +0100

    update build

--- vite.config.ts
+++ vite.config.ts
@@ -22,8 +22,8 @@ export default defineConfig({
     tsconfigPaths(),
   ],
   server: {
-    port: Number(process.env.PORT) || 3000,
-    host: '0.0.0.0'
+    port: process.env.PORT ? Number(process.env.PORT) : 3000,
+    host: "0.0.0.0"
   },
   build: {
     rollupOptions: {
